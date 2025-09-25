b6dca4ff751500d06d1a037a96184da8db42cb02
Enayat <enayatansari33@gmail.com>
2025-09-25 22:15:12 +0530

    add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import fetcherRoutes from "./routes/fetchRoutes";
 import alertRoutes from "./routes/alertRoutes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send("Crypto Price Monitor")
